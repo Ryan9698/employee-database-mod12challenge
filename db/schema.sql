44593011594f17215b957beb17babc8e1ec62e2a
Ryan Caltabiano <rcaltabiano90@gmail.com>
2023-11-16 18:48:40 -0500

    Added screenshot, finished code, edited README

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT NOT NULL, 
+  id INT AUTO_INCREMENT NOT NULL, 
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT NOT NULL, 
@@ -18,8 +18,10 @@ CREATE TABLE role (
   FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
+ALTER TABLE role MODIFY COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
+
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
@@ -28,4 +30,3 @@ CREATE TABLE employee (
   FOREIGN KEY(role_id) REFERENCES role(id),
   FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
-
